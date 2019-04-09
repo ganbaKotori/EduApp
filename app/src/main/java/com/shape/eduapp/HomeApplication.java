fc7901b7e60b7b778e7faed9f2a91ec443802863
Grenkappo <rollingrains@outlook.com>
2019-04-09 11:13:26 -0700

    HomeApplication added
    Added a Java class to ensure that the user is logged out properly.

---
+++ app/src/main/java/com/shape/eduapp/HomeApplication.java
@@ -0,0 +1,25 @@
+package com.shape.eduapp;
+
+import android.app.Application;
+import android.content.Intent;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/*
+    Checks current session of Firebase user
+ */
+public class HomeApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        //Send user back to ProfileActivity if user still logged in & verify user
+        if (firebaseUser != null && firebaseUser.isEmailVerified()) {
+            startActivity(new Intent(HomeApplication.this, ProfileActivity.class));
+        }
+    }
+}
