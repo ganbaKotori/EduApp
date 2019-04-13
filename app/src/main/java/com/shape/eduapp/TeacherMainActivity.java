747a37bcb04a5e0223f5d113b9d2e3ab06beb465
Alex <43049608+ganbaKotori@users.noreply.github.com>
2019-04-12 21:20:01 -0700

    Revert "Revert "added Course class and changed teacherActivity.xml""
    This reverts commit 982fca9b8432bed91c019b7ec718aa9963dd43a8.

--- app/src/main/java/com/shape/eduapp/TeacherMainActivity.java
+++ app/src/main/java/com/shape/eduapp/TeacherMainActivity.java
@@ -1,22 +1,44 @@
 package com.shape.eduapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import  android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class TeacherMainActivity extends AppCompatActivity {
     TextView tvUserEmail;
     Button btnLogout;
 
+    private ListView mCourseListView;
+    ArrayList<String> courses = new ArrayList<String>();
+
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mCourseDatabaseReference;
+    private ChildEventListener mChildEventListener;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +49,46 @@ public class TeacherMainActivity extends AppCompatActivity {
         btnLogout = findViewById(R.id.btnLogout);
 
         firebaseAuth = firebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
+
+        mCourseDatabaseReference = mFirebaseDatabase.getReference().child("courses");
+
+        // Initialize references to views
+        mCourseListView = (ListView) findViewById(R.id.courseListView);
+        final ArrayAdapter<String> courseArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,courses);
+        mCourseListView.setAdapter(courseArrayAdapter);
+
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Course myChildValues = dataSnapshot.getValue(Course.class);
+                courses.add(myChildValues.getTitle());
+                courseArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                courseArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mCourseDatabaseReference.addChildEventListener(mChildEventListener);
+
 
         tvUserEmail.setText(firebaseUser.getEmail());
 
